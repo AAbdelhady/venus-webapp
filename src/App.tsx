15a3f439be40de3d7e2467b4daef106d55c76935
ahmed@trytrue.com <ahmed@trytrue.com>
2020-01-22 16:14:02 +0200

    added initial redux and router config

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,26 @@ const App: React.FC = () => {
     axios.get('test').then(res => {
         console.log('API Call response', res.data);
     }).catch(err => console.warn(err));
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
-        <small>API base url environment variable value <b>{process.env.REACT_APP_API_BASE_URL}</b></small>
-      </header>
-    </div>
-  );
+    const googleLoginLink = `${process.env.REACT_APP_API_BASE_URL}/oauth2/authorization/google`;
+    const logoutLink = `${process.env.REACT_APP_API_BASE_URL}/logout`;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link"
+                   href="https://reactjs.org"
+                   target="_blank"
+                   rel="noopener noreferrer"> Learn React</a>
+                <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+                <small>API base url environment variable value <b>{process.env.REACT_APP_API_BASE_URL}</b></small>
+                <a href={googleLoginLink}>Google</a>
+                <a href={logoutLink}>Logout</a>
+            </header>
+        </div>
+    );
 };
 
 export default App;
