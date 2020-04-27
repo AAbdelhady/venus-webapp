2077aafa801f8ce8ed7eaccc62bf2ee55c1f4bec
ahmed@trytrue.com <ahmed@trytrue.com>
2020-04-27 21:44:26 +0300

    Cancellations

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,116 +1,71 @@
-import React, {Component} from "react";
-import {RouteComponentProps} from 'react-router';
-import {connect} from 'react-redux';
-import * as actions from '../../store/actions/index';
+import React, {useCallback, useEffect, useState} from "react";
+import {useDispatch, useSelector} from 'react-redux';
 import Layout from "../../hoc/layout/Layout";
 import {Pageable, Sort} from '../../models/pageable.model';
-import {Artist} from '../../models/artist.model';
 import ArtistCardList from './artist-card-list/ArtistCardList';
 import classes from './SearchPage.module.scss';
 import JumpToTop from '../../components/jump-to-top/JumpToTop';
 import {fetchArtistCategories} from '../../api/artist.api';
 import {Category} from '../../models/category.model';
 import Select from '../../components/ui/select/Select';
+import * as actions from '../../store/actions';
 
 const PAGE_SIZE = 20;
 
-interface Props {
-    artistList: Artist[];
-    totalElements: number;
-    currentPageNumber: number;
-    history: RouteComponentProps;
-    isFetchingPage: boolean;
-    searchArtists(pageable: Pageable, category: string | null);
-}
-
-interface State {
-    currentCategory: string | null;
-    categories: Category[];
-}
-
-class SearchPage extends Component<Props> {
-
-    state: State = {
-        currentCategory: null,
-        categories: []
+const fetchPage = (pageNumber, category, dispatch) => {
+    const sort: Sort = {
+        field: 'id',
+        direction: 'desc'
     };
+    const pageable = new Pageable(pageNumber, PAGE_SIZE, sort);
+    dispatch(actions.searchArtists(pageable, category));
+};
 
-    componentDidMount(): void {
-        this.fetchCategories();
-    }
+const SearchPage = () => {
+    const dispatch = useDispatch();
+    const [currentCategory, setCurrentCategory] = useState<string | null>(null);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const {artistList, currentPageNumber, totalElements, loading} = useSelector(state => state.artist);
 
-    fetchCategories() {
+    const startOver = useCallback((category) => fetchPage(0, category, dispatch), [dispatch]);
+    const nextPage = () => fetchPage(currentPageNumber + 1, currentCategory, dispatch);
+
+    useEffect(() => {
         fetchArtistCategories().then(res => {
-            this.setState({
-                categories: res.data,
-                currentCategory: res.data[0].value
-            });
-            this.startOver(res.data[0].value);
-        });
-    }
+            setCategories(res.data);
+            setCurrentCategory(res.data[0].value);
+            startOver(res.data[0].value);
+        })
+    }, [startOver]);
 
-    startOver = (category) => {
-        this.fetchPage(0, category);
-    };
-
-    nextPage = () => {
-        this.fetchPage(this.props.currentPageNumber + 1, this.state.currentCategory);
-    };
-
-    refresh = () => {
-        this.startOver(this.state.currentCategory);
-    };
-
-    onCategoryChanged = (event) => {
+    const onCategoryChanged = (event) => {
         const newCategory = event.target.value;
-        this.setState({currentCategory: newCategory});
-        this.startOver(newCategory);
+        setCurrentCategory(newCategory);
+        startOver(newCategory);
     };
 
-    fetchPage = (pageNumber, category) => {
-        const sort: Sort = {
-            field: 'id',
-            direction: 'desc'
-        };
-        const pageable = new Pageable(pageNumber, PAGE_SIZE, sort);
-        this.props.searchArtists(pageable, category);
-    };
-
-    get hasMoreResults() {
-        return this.props.artistList.length !== this.props.totalElements;
-    }
-
-    render() {
-        const artists = this.props.artistList || [];
-        return (
-            <Layout>
-                <div className={classes.Container}>
-                    <div className={classes.CategorySelectContainer}>
-                        <Select name="specialityId"
-                                options={this.state.categories}
-                                value={this.state.currentCategory}
-                                onChange={this.onCategoryChanged}
-                                textProperty="text"
-                                valueProperty="value"
-                        />
-                    </div>
-                    <ArtistCardList artistList={artists} hasMore={this.hasMoreResults} nextPage={this.nextPage} refresh={this.refresh} loading={this.props.isFetchingPage} pageSize={PAGE_SIZE}/>
+    return (
+        <Layout>
+            <div className={classes.Container}>
+                <div className={classes.CategorySelectContainer}>
+                    <Select name="specialityId"
+                            options={categories}
+                            value={currentCategory}
+                            onChange={onCategoryChanged}
+                            textProperty="text"
+                            valueProperty="value"
+                    />
                 </div>
-                <JumpToTop/>
-            </Layout>
-        );
-    }
+                <ArtistCardList artistList={artistList || []}
+                                hasMore={artistList.length !== totalElements}
+                                nextPage={nextPage}
+                                refresh={() => startOver(currentCategory)}
+                                loading={loading}
+                                pageSize={PAGE_SIZE}/>
+            </div>
+            <JumpToTop/>
+        </Layout>
+    );
 }
 
-const mapStateToProps = state => ({
-    artistList: state.artist.artistList,
-    currentPageNumber: state.artist.currentPageNumber,
-    totalElements: state.artist.totalElements,
-    isFetchingPage: state.artist.loading
-});
-
-const mapDispatchToProps = dispatch => ({
-    searchArtists: (pageable, category) => dispatch(actions.searchArtists(pageable, category))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchPage);
+export default SearchPage;
